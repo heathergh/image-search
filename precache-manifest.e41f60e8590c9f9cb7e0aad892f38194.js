6b873c5a750b281592e1ec9a4da88d5d82330ba6
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-12-03 13:32:00 -0500

    Updates

---
+++ precache-manifest.e41f60e8590c9f9cb7e0aad892f38194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1fa19495bcdf726739a44517b88b1e",
+    "url": "/heather-hamilton-project-five/index.html"
+  },
+  {
+    "revision": "e8ea868693de26eda7ed",
+    "url": "/heather-hamilton-project-five/static/css/main.0c4f1579.chunk.css"
+  },
+  {
+    "revision": "708b93e73e1b84e19704",
+    "url": "/heather-hamilton-project-five/static/js/2.c04624b9.chunk.js"
+  },
+  {
+    "revision": "e8ea868693de26eda7ed",
+    "url": "/heather-hamilton-project-five/static/js/main.0f06c1e6.chunk.js"
+  },
+  {
+    "revision": "9ce3d06ca7f320984db2",
+    "url": "/heather-hamilton-project-five/static/js/runtime-main.211c0f16.js"
+  }
+]);
