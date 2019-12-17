c110b545abcdbca9cbcd1f74b9cf49c34caa7161
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-12-17 08:51:03 -0500

    Changed colours and added instructions with unsplash accredition

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = props => {
     return (
         <header>
             <h1>Image Search</h1>
+            <p>Search for an image in the input below. Download your favourite ones or visit the original page. Made the with the Unsplash API.</p>
         </header>
     )
 }
