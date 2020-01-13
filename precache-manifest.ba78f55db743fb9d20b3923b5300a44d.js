f2856edcd3ebfa71aa9b0195ce8d1eb264b20416
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-01-13 17:49:24 -0500

    Updates

---
+++ precache-manifest.ba78f55db743fb9d20b3923b5300a44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28828429a80f793381072b9a5aee2c7b",
+    "url": "/image-search/index.html"
+  },
+  {
+    "revision": "48d2cb45357233fdb1ad",
+    "url": "/image-search/static/css/main.3139d641.chunk.css"
+  },
+  {
+    "revision": "98029bc98e9265338da0",
+    "url": "/image-search/static/js/2.714163bc.chunk.js"
+  },
+  {
+    "revision": "48d2cb45357233fdb1ad",
+    "url": "/image-search/static/js/main.6d12de26.chunk.js"
+  },
+  {
+    "revision": "6a0bd75058387fa52e84",
+    "url": "/image-search/static/js/runtime-main.d6ad162f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/heather-hamilton-project-five/precache-manifest.2b6aa4f2d00b63864e43ba75f2468078.js"
+  "/image-search/precache-manifest.ba78f55db743fb9d20b3923b5300a44d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/heather-hamilton-project-five/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/image-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
